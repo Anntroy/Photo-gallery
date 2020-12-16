147ea3d33b7ba21739bbdeaeef02527a5f38c785
Anntroy <ana.troyan@gmail.com>
2020-12-16 17:00:30 +0100

    grid structure for main view

---
+++ data.js
@@ -0,0 +1,16 @@
+localStorage.setItem(
+    "gallery",
+    JSON.stringify([
+    { src: "assets/images/alfaz01.jpg", alt: "alfaz", keyword: "lighthouse", date: 1607698630012},
+    { src: "assets/images/alfaz02.jpg", alt: "alfaz", keyword: "lighthouse", date: 1607698630012},
+    { src: "assets/images/benidorm01.jpg", alt: "benidorm01.jpg", keyword: "beach", date: 1607698536006},
+    { src: "assets/images/cherniv01.jpg", alt: "cherniv01.jpg", keyword: "city", date: 1607698506014},
+    { src: "assets/images/cherniv02.jpg", alt: "cherniv02.jpg", keyword: "city", date: 1607698506014},
+    { src: "assets/images/cherniv03.jpg", alt: "cherniv03.jpg", keyword: "city", date: 1607698506014},
+    { src: "assets/images/cherniv04.jpg", alt: "cherniv04.jpg", keyword: "city", date: 1607698506014},
+    { src: "assets/images/paradise01.jpg", alt: "paradise01.jpg", keyword: "bicycle", date: 1607698616000},
+    { src: "assets/images/paradise02.jpg", alt: "paradise02.jpg", keyword: "bicycle", date: 1607698616000},
+    { src: "assets/images/paradise03.jpg", alt: "paradise03.jpg", keyword: "bicycle", date: 1607698616000},
+    { src: "assets/images/suances01.jpg", alt: "suances01", keyword: "sea", date: 1607698306042},
+])
+);
---
+++ main.js
@@ -0,0 +1,2 @@
+const get = document.getElementById.bind(document);
+const gallery = get("main-gallery-view");
