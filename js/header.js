6cd1a19ed28678ff768beca738d803f349214199
Anntroy <ana.troyan@gmail.com>
2020-12-19 19:44:36 +0100

    close sign up form

--- js/header.js
+++ js/header.js
@@ -2,9 +2,15 @@ function responsiveTopBar(){
     window.addEventListener('resize', function(){
         if (window.matchMedia("(min-width: 600px)").matches) {
             title.innerHTML = `<h1 class="header__button-h1">Photo Gallery</h1>`;
+            aside.classList.remove('hidden');
+            signUp.classList.remove('hidden');
+            signIn.classList.remove('hidden');
         }
         else {
             title.innerHTML = `<h1 class="header__button-h1">Photo</h1>`;
+            aside.classList.add('hidden');
+            signUp.classList.add('hidden');
+            signIn.classList.add('hidden');
         }
         // if (window.matchMedia("(min-width: 1000px)").matches) {
         //     headerRight.innerHTML = `<form class="" action="">
--- js/signUp.js
+++ js/signUp.js
@@ -1,27 +1,17 @@
 function openSignUpModal(){
     signUp.addEventListener('click', function(){
-        modal.innerHTML = `<form class="modal__form">
-                                <div class="modal__form-div">
-                                    <h2 class="modal__form-div-h2">Sign Up</h2>
-                                    <hr>
-                                    <label class="modal__form-div-label" for="email"><b>Email</b></label>
-                                    <input class="modal__form-div-input" type="text" placeholder="Enter Email" name="email" required>
-
-                                    <label class="modal__form-div-label" for="psw"><b>Password</b></label>
-                                    <input class="modal__form-div-input" type="password" placeholder="Enter Password" name="psw" required>
-
-                                    <label class="modal__form-div-label" for="psw-repeat"><b>Repeat Password</b></label>
-                                    <input class="modal__form-div-input" type="password" placeholder="Repeat Password" name="psw-repeat" required>
-                                    <div class="clearfix">
-                                    <button type="button" onclick="document.getElementById('id01').style.display='none'" class="btn cancelbtn">Cancel</button>
-                                    <button type="submit" class="btn signupbtn">Sign Up</button>
-                                    </div>
-                                    </div>
-                                    </form>`
+        modal.classList.remove('hidden');
     })
 }
 openSignUpModal();
 
-{/* <label>
-    <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-</label> */}
+function closeSignUpModal(){
+    cancelbtn.addEventListener('click', function(){
+        modal.classList.add('hidden');
+    });
+    signupbtn.addEventListener('click', function(){
+        modal.classList.add('hidden');
+    });
+}
+
+closeSignUpModal();
